3d65580c8744fc4818ef706680c584c944ce64b3
Carrie Wladis <carriewladis@gmail.com>
2020-05-20 09:37:15 -0400

    playing with status

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
 	res.send('Hello World from my server!');
 });
 
+app.get('/test', (req, res) => {
+	res.set('X-Custom-header', 'I am a custom header');
+	res.status(418);
+	res.send('header and status set on test')
+})
+
 app.listen(port, () => 	console.log(`Listening on port ${port}`));
 
 
